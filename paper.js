dcb0f91faf96d62b5aa32cebc8d0ce3fc64362d9
Karine <karine.jamet203@gmail.com>
2015-12-13 09:01:49 +0100

    first commit

---
+++ paper.js
@@ -0,0 +1,22 @@
+
+window.onload = function(){
+
+
+	consol.log("hello");
+	var path = new Path();
+	path.strokeColor = 'black';
+	var start = new Point(100, 100);
+		
+	path.moveTo(start);
+	path.lineTo(start + [ 100, -50 ]);
+
+	var path = new Path.Circle({
+		center: view.center,
+		radius: 30,
+		strokeColor: 'black'
+	});
+
+	function onResize(event) {
+		path.position = view.center;
+	}
+};
